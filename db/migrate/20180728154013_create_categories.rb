1f14cad7ffc986d7bc4bca6ab3bb1403b2573a69
James Hiller <hillerj1@gmail.com>
2018-07-28 12:53:37 -0400

    total file replacement

--- db/migrate/20180728154013_create_categories.rb
+++ db/migrate/20180728154013_create_categories.rb
@@ -1,6 +1,11 @@
 class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
+      t.string :apps_and_snacks
+      t.string :breakfast_and_brunch
+      t.string :dinner
+      t.string :desserts
+      t.string :drinks
 
       t.timestamps
     end
