130dc3ff12160017bfe71d16515707e02f481887
nbknight <nicholas.b.knight@gmail.com>
2018-08-01 09:24:08 -0400

    Add logic to search by tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
 
-	before_action :categories
+	before_action :categories, :tag
 
 	def categories
 		@categories = Category.all
+	end
+
+	def tag 
+		@tag = Tag.all
 	end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    @tag = Tag.all
   end
 
   # GET /categories/1
